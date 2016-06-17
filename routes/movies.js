26e695bfcc0d23650f762f8bc6f7e93d8ac299fb
Jeremy <jkaffrey@gmail.com>
2016-06-17 14:33:52 -0600

    Sorting via type now works

--- routes/movies.js
+++ routes/movies.js
@@ -5,6 +5,28 @@ router  = express.Router(),
 pg      = require('pg'),
 db_url  = process.env.DATABASE_URL || 'postgres://localhost:5432/movie_inventory_crud';
 
+function sortBy(toSort, queryObj) {
+
+  var soryBy = Object.keys(queryObj)[0];
+  var aD = (queryObj[soryBy] === 'asc' ? 1 : -1);
+  return toSort.sort(function(a, b) {
+
+    var aVar, bVar;
+
+    switch (soryBy) {
+      case 'title': aVar = a.title.toLowerCase(); bVar = b.title.toLowerCase(); break;
+      case 'year': aVar = a.year; bVar = b.year; break;
+      case 'date': aVar = a.date_obtained.toLowerCase(); bVar = b.date_obtained.toLowerCase(); break;
+      case 'rating': aVar = a.rating; bVar = b.rating; break;
+      case 'type': aVar = a.type.toLowerCase; bVar = b.type.toLowerCase(); break;
+    }
+
+    if (aVar > bVar) return -1 * aD;
+    else if (aVar < bVar) return 1 * aD;
+    else return 0;
+  });
+}
+
 router.get('/', function(req, res) {
 
   pg.connect(db_url, function(err, client, done) {
@@ -18,7 +40,12 @@ router.get('/', function(req, res) {
         if (err) console.log(err);
         else {
 
-          res.render('./index', {movies: data.rows});
+          if (Object.keys(req.query).length === 0) {
+            res.render('./index', {movies: data.rows});
+          }
+          else {
+            res.render('./index', {movies: sortBy(data.rows, req.query)});
+          }
         }
       });
     }
