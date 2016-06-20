d4f7ba14b430c0f5f39ce0fba94545b113b5d81f
Jeremy <jkaffrey@gmail.com>
2016-06-20 14:35:33 -0600

    Removed comments

--- routes/movies.js
+++ routes/movies.js
@@ -39,28 +39,6 @@ router.get('/', function(req, res) {
 
     res.render('./index', { movies: data });
   });
-  // pg.connect(db_url, function(err, client, done) {
-  //
-  //   done();
-  //   if (err) console.log(err);
-  //   else {
-  //
-  //
-  //     client.query('SELECT * FROM movies', function(err, data) {
-  //
-  //       if (err) console.log(err);
-  //       else {
-  //
-  //         if (Object.keys(req.query).length === 0) {
-  //           res.render('./index', {movies: data.rows});
-  //         }
-  //         else {
-  //           res.render('./index', {movies: sortBy(data.rows, req.query)});
-  //         }
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 router.get('/new', function(req, res) {
@@ -77,24 +55,6 @@ router.get('/:id', function(req, res) {
 
     console.log(err);
   });
-  // pg.connect(db_url, function(err, client, done) {
-  //
-  //   done();
-  //   if (err) console.log(err);
-  //   else {
-  //
-  //     client.query('SELECT * FROM movies WHERE id = $1', [req.params.id], function(err, data) {
-  //
-  //       if (err) console.log(err);
-  //       else {
-  //
-  //         var movie = data.rows[0];
-  //         if (!movie) res.redirect('');
-  //         res.render('./preview', { movie: movie });
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 router.get('/:id/edit', function(req, res) {
@@ -107,28 +67,6 @@ router.get('/:id/edit', function(req, res) {
 
     console.log(err);
   });
-  // pg.connect(db_url, function(err, client, done) {
-  //
-  //   done();
-  //   if (err) console.log(err);
-  //   else {
-  //
-  //     client.query('SELECT * FROM movies WHERE id=$1', [req.params.id], function(err, data) {
-  //
-  //       if (err) console.log(err);
-  //       else {
-  //
-  //         var movie = data.rows[0];
-  //         if (!movie) {
-  //
-  //           res.redirect('/');
-  //         }
-  //
-  //         res.render('edit', { movie: movie });
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 router.post('/', function(req, res) {
@@ -150,32 +88,6 @@ router.post('/', function(req, res) {
 
     console.log('ERROR: ' + err);
   });
-  // pg.connect(db_url, function(err, client, done) {
-  //
-  //   done();
-  //   if (err) console.log(err);
-  //   else {
-  //
-  //     console.log(req.body);
-  //     client.query('INSERT INTO movies VALUES(default, $1, $2, $3, $4, $5, $6, $7, $8)', [
-  //       req.body.title,
-  //       req.body.description,
-  //       req.body.img_url,
-  //       req.body.year,
-  //       req.body.date_obtained,
-  //       req.body.rating,
-  //       req.body.notes,
-  //       req.body.type
-  //     ], function(err, data) {
-  //
-  //       if (err) console.log(err);
-  //       else {
-  //
-  //         res.redirect('/');
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 router.post('/:id/edit', function(req, res) {
@@ -195,32 +107,6 @@ router.post('/:id/edit', function(req, res) {
 
     res.redirect('/');
   });
-  // pg.connect(db_url, function(err, client, done) {
-  //
-  //   done();
-  //   if (err) console.log(err);
-  //   else {
-  //
-  //     client.query('UPDATE movies SET ( title, description, img_url, year, date_obtained, rating, notes, type ) = ( $2, $3, $4, $5, $6, $7, $8, $9) WHERE id = $1', [
-  //       req.params.id,
-  //       req.body.title,
-  //       req.body.description,
-  //       req.body.img_url,
-  //       req.body.year,
-  //       req.body.date_obtained,
-  //       req.body.rating,
-  //       req.body.notes,
-  //       req.body.type
-  //     ], function(err, data) {
-  //
-  //       if (err) console.log(err);
-  //       else {
-  //
-  //         res.redirect('/');
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 router.post('/:id/delete', function(req, res) {
@@ -230,22 +116,6 @@ router.post('/:id/delete', function(req, res) {
     console.log(data);
     res.redirect('/');
   });
-  // pg.connect(db_url, function(err, client, done) {
-  //
-  //   done();
-  //   if (err) console.log(err);
-  //   else {
-  //
-  //     client.query('DELETE FROM movies WHERE id = $1', [req.params.id], function(err, data) {
-  //
-  //       if (err) console.log(err);
-  //       else {
-  //
-  //         res.redirect('/');
-  //       }
-  //     });
-  //   }
-  // });
 });
 
 module.exports = router;
